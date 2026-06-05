e65b22c5f11c1defa2c240b2dae63db33e7f4bb5
padipan <padipan.pantong@gmail.com>
2026-06-05 22:35:00 +0700

    Configured for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // จำเป็นมาก เพื่อป้องกันไม่ให้ระบบรูปภาพของ Next.js พังบน GitHub Pages
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
